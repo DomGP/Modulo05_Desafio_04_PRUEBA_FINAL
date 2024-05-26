ef060a3382f5be1f36924e2626eceafcebf92763
Dominique Gajardo <dominique.gajardoap@gmail.com>
2024-05-25 22:22:21 -0400

    Arreglo pagina comodin

--- src/views/Comodin.jsx
+++ src/views/Comodin.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Comodin = () => {
     return (
         <div className='container'>
+            <img src='/assets/imgs/notfound.jpg' alt="Imagen no encontrada" />
             <img src='./src/assets/imgs/notfound.jpg' alt="Imagen no encontrada" />
         </div>
     );
